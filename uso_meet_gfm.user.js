be08efb598baa44466b0275a6ae47a40146e7cbb
Tim-Smart <timehAndGod@gmail.com>
2010-03-09 16:06:52 +1300

    Initial commit

---
+++ uso_meet_gfm.user.js
@@ -0,0 +1,6 @@
+// ==UserScript==
+// @name           USO, meet GFM.
+// @namespace      http://userscripts.org/users/tim
+// @description    GitHub Markdown gets married to Userscript.org
+// @include        http://userscripts.org/topics/*
+// ==/UserScript==
